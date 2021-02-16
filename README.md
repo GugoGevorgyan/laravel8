5b646856c13fb1a252df0f60f265c09248bb1552
Gugo <gugo1519@gmail.com>
2021-02-16 19:02:15 +0400

    logika

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 composer install
 
 1.copy .env.example and create .env file or "copy .env.example .env"
-php artisan storage:link
-npm install
-npm run dev
-php artisan key:generate
+
+2.php artisan storage:link
+
+3.npm install
+
+4.npm run dev
+
+5.php artisan key:generate
+
+6.php artisan storage:link
+
+7.php artisan migrate --seed
+
+8.
+
 php artisan serv
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'SuperAdmin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'admin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+
+        DB::table('users')->truncate();
+        DB::table('users')->insert(array (
+            0 =>
+                [
+                    'email' => 'superadmin@gmail.com',
+                    'name' => 'Super',
+                    'password' => Hash::make('1111'),
+                    'role_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+        ));
+    }
+}
