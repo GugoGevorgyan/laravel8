4bc032e60f6e6beda82c2bd4511fd41f4029c4d3
Gugo <gugo1519@gmail.com>
2021-03-22 17:18:37 +0400

    merge Cart ->dev

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,9 +17,7 @@ class Category extends Model
     public function subCategory(){
         return $this->hasMany(Category::class, 'parent_id','id');
     }
-//    public function products(){
-//                return $this->hasMany(Product::class);
-//    }
+
     public function products(){
         return $this->belongsToMany(Product::class);
     }
